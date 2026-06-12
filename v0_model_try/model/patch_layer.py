884381b0706d023172d9b6eada85250d5ac49b76
Ishaan Datta <ishaandatta19@gmail.com>
2026-06-12 12:15:56 +0530

    Update: Patch layer fixes, no more num_layers nonsense, num_layers inferred from freq_hop and kernel size

--- v0_model_try/model/patch_layer.py
+++ v0_model_try/model/patch_layer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import sys
 import math
 
-from ..config import CLIP_SAMPLES, HOP_LENGTH, N_BINS, BATCH_SIZE
+from config import CLIP_SAMPLES, HOP_LENGTH, N_BINS, BATCH_SIZE, EMB_DIM
 
 def give_nn_conv_layer(in_channels, out_channels, kernel_size, stride):
     return nn.Conv2d(
@@ -11,7 +11,7 @@ def give_nn_conv_layer(in_channels, out_channels, kernel_size, stride):
         out_channels=out_channels,
         kernel_size=kernel_size,
         stride=stride,
-        padding=(0, 1), # No padding, we want to reduce the dimensions
+        padding=(0, 0), # Padding to retain dimensions in time dim 
     )
 
 class PatchLayer(nn.Module):
@@ -21,34 +21,32 @@ class PatchLayer(nn.Module):
     # Conv2d gives (B, d_model, H_out, W_out), which we then cast to (B, T_patches, d_model) by permuting and reshaping
 
     def __init__(self, 
-                 freq_dim=N_BINS, 
-                 time_dim=CLIP_SAMPLES // HOP_LENGTH + 1, 
-                 n_layers=3,
+                 freq_dim = N_BINS, 
+                 time_dim = CLIP_SAMPLES // HOP_LENGTH + 1,
                  # dimensions
-                 emb_dim=128,
+                 emb_dim=EMB_DIM, # Desired embedding dimension for each patch (d_model)
                  # Conv param
                  kernel_size=(2, 2),
-                time_hop=1
+                 frequency_hop=2,
+                 time_hop=1
                  ):
         super().__init__()
         self.freq_dim = freq_dim # Frequency dimension of the patch (number of mel bins)
         self.time_dim = time_dim # Time dimension of the patch (number of time frames)
         self.emb_dim = emb_dim # Desired embedding dimension for each patch(d_model)
-        self.time_hop = time_hop
+        self.time_hop = min(time_hop, kernel_size[1]) # Ensure time_hop does not exceed kernel size in time dimension
+        self.frequency_hop = min(frequency_hop, kernel_size[0]) # Ensure frequency_hop does not exceed kernel size in frequency dimension
         
         
         self.layers = []
 
-        print(f"Time dimension // time hop: {math.log(time_dim // time_hop, 2)}")
-        print(f"Initial number of layers requested: {n_layers}")
-        n_layers = min(n_layers, math.log(time_dim // time_hop, 2) + 1) # Ensure we don't exceed the dimensions of the input
-        n_layers = int(n_layers) # Convert to integer
+        n_layers = int(math.log(freq_dim, frequency_hop)) # Number of layers needed to reduce freq_dim to 1
         print("Adjusted number of layers based on input dimensions:", n_layers)
         
         channel_dims = [(emb_dim//2**i) for i in range(n_layers)][::-1] # Multiply dimensions by 2 every layer, only for the first half of the layers, then keep it constant
         print("Channel dimensions for each layer:", channel_dims)
         
-        self.freq_hop = freq_dim // (2 ** (n_layers - 1)) # Calculate the frequency hop based on the number of layers and the input frequency dimension
+        
         for i in range(n_layers):
             # 
             self.layers.append(
@@ -56,7 +54,7 @@ class PatchLayer(nn.Module):
                     in_channels=1 if i == 0 else channel_dims[i - 1],
                     out_channels=channel_dims[i] ,
                     kernel_size=kernel_size,
-                    stride=(self.freq_hop, 1)
+                    stride=(frequency_hop, time_hop)
                 )
             )
             self.layers.append(nn.ReLU())
@@ -80,7 +78,7 @@ class PatchLayer(nn.Module):
     
 if __name__ == "__main__":
     # Local testing
-    patch_layer = PatchLayer(n_layers=2)
+    patch_layer = PatchLayer()
     dummy_input = torch.randn(BATCH_SIZE, N_BINS, CLIP_SAMPLES // HOP_LENGTH + 1) # (B, F, T)
     print("Dummy input shape:", dummy_input.shape)
     output = patch_layer(dummy_input)
