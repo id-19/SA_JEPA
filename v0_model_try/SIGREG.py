ee71c07622fde91e0fddecaa1fa0a0876cd0b779
Ishaan Datta <ishaandatta19@gmail.com>
2026-08-01 22:31:04 +0530

    Built and tested unit vector creation

--- v0_model_try/SIGREG.py
+++ v0_model_try/SIGREG.py
@@ -6,7 +6,8 @@ import torch
 def create_unit_vectors(dimension: int, num_unit_vecs: int) -> list[torch.Tensor]:
     unit_vectors = []
     for _ in range(num_unit_vecs):
-        unit_vectors.append(torch.randn()) # TODO implement this
+        # Create unit vectors of same dimension as latent
+        unit_vectors.append(torch.randn(dimension)) # Broadcasting will automatically infer earlier dimensions(i.e. matches right to left so (Z, K) and (K) can be multiplied as (Z,K) and (1, K))
     return unit_vectors
 
 
@@ -15,3 +16,7 @@ def project_latents(latents: list[torch.Tensor]) -> torch.Tensor:
     unit_vectors = create_unit_vectors(latent_dimension, int(len(latents) / 10)) # 0.1 as many unit vectors as latents
     # TODO: Implement this
     return torch.randn()
+
+if __name__ == '__main__':
+    unit_vectors = create_unit_vectors(512, 10)
+    print(unit_vectors[0].shape)
