7241e6ef806e2981c98a31e9e8ce6802a6a44ff6
Ishaan Datta <ishaandatta19@gmail.com>
2026-09-04 08:12:00 +0530

    Add Mamba v0 implementation

--- v0_model_try/model/mamba.py
+++ v0_model_try/model/mamba.py
@@ -20,16 +20,46 @@ class SSM(nn.Module):
 
         # Record the parameters
         self.A = torch.randn((d_state, d_state))
-        self.B = torch.randn((d_input, d_latent))
+        self.B = torch.randn((d_latent, d_state))
         self.C = torch.randn((d_state, d_output))
+        # Latent projection as well
+        self.proj = nn.Linear(d_input, d_latent)
+
 
     def forward(self, x: torch.Tensor):
+        if x.dim == 3:
+            B,T,C = x.shape
+        else:
+            T,C = x.shape
+            B = 1
+        x = x.view(B*T, C)
+
         if x.shape[-1] != self.d_input:
             raise Exception("Input to SSM is not of the expected dimensions")
 
+        # Project input
+        x = self.proj(x)
+
         # Update state
-        self.state = self.A @ self.state + self.B @ x
+        # print("Initial shape of state", self.state.shape)
+        for eff_timestep in range(x.shape[0]):
+            # print(f"State at timestep, {eff_timestep} is: {self.state}")
+            self.state = self.A @ self.state # (d_state, d_state) @ (d_state, 1) = (d_state)
+            self.state += x[eff_timestep] @ self.B # (1, d_latent) @ (d_latent, d_state) = (d_state)
+            # print("Output at ", eff_timestep, ": ", self.state @ self.C)
+        # print("Final shape of state", self.state.shape)
 
         # Calculate output
-        output = self.C @ self.state
+        output = self.state @ self.C
         return output
+
+def test_const_delta_ssm(d_input = 10, d_latent = 5, d_state = 10, d_output = 5, inputs = torch.randn((10, 10))):
+    ssm = SSM(d_input=d_input, d_latent=d_latent, d_state=d_state, d_output=d_output)
+    # Go thru the inputs one by one
+    print("Shape of state initially", ssm.state.shape)
+    output = ssm(inputs)
+    print("Final state", ssm.state.shape)
+    print("Output", output)
+
+if __name__ == "__main__":
+    test_const_delta_ssm()
